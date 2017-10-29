f128c62f9a6c3c63f962b2de611f4adc7bbd2fda
Mohamad Zbib <toffahtarec@gmail.com>
2017-10-29 09:41:00 +0200

    preparing to test

--- test.c
+++ test.c
@@ -3,9 +3,11 @@
 
 int main(){
 
-	int lay[5]={3,4,5,3,1};
+	int lay[5]={2,4,5,3,1};
 	int i;
-	double inp[3];
+	int steps=0;
+	double inp[2],tar[1];
+
 	double * outp;
 	if(INIT_NETWORK(lay,5)!=0){
 		printf("not working INIT_NETWORK\n");
@@ -13,11 +15,24 @@ int main(){
 	}
 	else 
 		printf("Network Initailized\n");
+	while(1){
+		printf("enter 2 data to your network:\n");
+		for(i=0;i<2;i++)
+			scanf("%lf",&inp[i]);
+		printf("enter your target value:\n");
+		scanf("%lf",&tar[0]);
 
-	printf("enter 3 data to your network:\n");
-	for(i=0;i<3;i++)
-		scanf("%lf",&inp[i]);
-	outp=forward(inp);
-	printf("the output:%g\n",*outp);
+		outp=forward(inp);
+		printf("the output:%g\n",*outp);
+
+		while(cost_fx(tar)>0.005)
+		{
+			printf("correction number %d.\n",++steps);
+			printf("\tcost_fx=%lf\n",cost_fx(tar));
+			backProp(tar);
+			printf("new output:%g\n",*forward(inp));
+		}
+	}
+
 	return 0;
 }
